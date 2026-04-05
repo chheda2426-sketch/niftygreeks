dc079b0eb95d6accaf2376e316c68322ecb5e813
chheda2426-sketch <chheda2426@gmail.com>
2026-04-05 16:48:29 +0530

    Update route.ts

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,10 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
 import { NextRequest, NextResponse } from 'next/server'
+import { createClient } from '@supabase/supabase-js'
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
-  const origin = requestUrl.origin
 
   if (code) {
     const supabase = createClient(
@@ -14,5 +13,5 @@ export async function GET(request: NextRequest) {
     await supabase.auth.exchangeCodeForSession(code)
   }
 
-  return NextResponse.redirect(${origin}/dashboard)
+  return NextResponse.redirect(new URL('/dashboard', request.url))
 }
